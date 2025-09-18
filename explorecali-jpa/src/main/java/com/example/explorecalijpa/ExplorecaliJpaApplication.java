329f8f4f041f354284c3cc9ce6264a1c0e9179f9
Antonio Casado <antonio1313@gmail.com>
2025-09-18 22:25:49 +0200

    Update ExplorecaliJpaApplication.java

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/ExplorecaliJpaApplication.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/ExplorecaliJpaApplication.java
@@ -8,6 +8,7 @@ public class ExplorecaliJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExplorecaliJpaApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
